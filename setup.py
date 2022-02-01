4c6ea202ee76a0d75f3531b9c1121caa24fee587
Anthony <anthony.soulain@protonmail.com>
2022-02-01 16:51:14 +0100

    Add new setup cfg

--- setup.py
+++ setup.py
@@ -1,31 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-project_name = "oimalib"
-
-setup(
-    name=project_name,
-    version=0.1,
-    packages=find_packages(),
-    author="Anthony Soulain",
-    author_email="anthony.soulain@protonmail.com",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Professional Astronomers",
-        "Topic :: High Angular Resolution Astronomy :: Interferometry",
-        "Programming Language :: Python :: 3.9",
-    ],
-    install_requires=[
-        "matplotlib",
-        "munch",
-        "numpy",
-        "emcee",
-        "astropy",
-        "scipy",
-        "termcolor",
-        "tqdm",
-        "uncertainties",
-        "astroquery",
-        "corner",
-        "pytest",
-    ],
-)
+setup()
